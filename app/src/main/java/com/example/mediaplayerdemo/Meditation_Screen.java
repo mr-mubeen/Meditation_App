e7c0747c8b6adc1e0135279e0b1f459638e15d70
mr-mubeen <mubeen13132@gmail.com>
2022-02-09 20:39:25 +0500

    updated

--- app/src/main/java/com/example/mediaplayerdemo/Meditation_Screen.java
+++ app/src/main/java/com/example/mediaplayerdemo/Meditation_Screen.java
@@ -2,13 +2,52 @@ package com.example.mediaplayerdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Meditation_Screen extends AppCompatActivity {
 
+
+    LinearLayout l1,l2,l3,l4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meditation__screen);
+
+        l1 = findViewById(R.id.anxiety_id);
+        l2 = findViewById(R.id.depression_id);
+        l3 = findViewById(R.id.sleep_id);
+        l4 = findViewById(R.id.focus_id);
+
+        l1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext() , Medtation_QA.class);
+                startActivity(intent);
+            }
+        });
+        l2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext() , Medtation_QA.class);
+                startActivity(intent);
+            }
+        });
+        l3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext() , Medtation_QA.class);
+                startActivity(intent);
+            }
+        });
+        l4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext() , Medtation_QA.class);
+                startActivity(intent);
+            }
+        });
     }
 }
