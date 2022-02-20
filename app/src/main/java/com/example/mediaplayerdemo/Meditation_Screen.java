49414795d55e60ea9c739879fcfad0e188483562
mr-mubeen <mubeen13132@gmail.com>
2022-02-21 03:59:46 +0500

    updated

--- app/src/main/java/com/example/mediaplayerdemo/Meditation_Screen.java
+++ app/src/main/java/com/example/mediaplayerdemo/Meditation_Screen.java
@@ -10,7 +10,7 @@ import android.widget.LinearLayout;
 public class Meditation_Screen extends AppCompatActivity {
 
 
-    LinearLayout l1,l2,l3,l4;
+    LinearLayout l1,l2,l3,l4,l5,l6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +20,8 @@ public class Meditation_Screen extends AppCompatActivity {
         l2 = findViewById(R.id.depression_id);
         l3 = findViewById(R.id.sleep_id);
         l4 = findViewById(R.id.focus_id);
+        l5 = findViewById(R.id.bipolar);
+        l6 = findViewById(R.id.psychosis);
 
         l1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +45,21 @@ public class Meditation_Screen extends AppCompatActivity {
             }
         });
         l4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext() , Medtation_QA.class);
+                startActivity(intent);
+            }
+        });
+
+        l5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext() ,);
+                startActivity(intent);
+            }
+        });
+        l6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext() , Medtation_QA.class);
---
+++ app/src/main/java/com/example/mediaplayerdemo/Psychosis.java
@@ -0,0 +1,14 @@
+package com.example.mediaplayerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Psychosis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_psychosis);
+    }
+}
