00fc4d3e367d15a3458e6b2d413dd4978f18fe3b
mr-mubeen <mubeen13132@gmail.com>
2022-02-22 20:14:20 +0500

    updated

---
+++ Database_Api's/addiction_points.php
@@ -0,0 +1,20 @@
+<?php
+
+require ('connect.php');
+
+
+$points = $_POST["p"];
+$rid = $_POST["rid"];
+
+$rrid = (int)$rid;
+
+
+$sql = "INSERT INTO point ( addiction_points, r_id) VALUES ('$points' , '$rrid')";
+$result = mysqli_query($conn , $sql) or die ("Regsiter Failed");
+
+if ($result) {
+  echo "Registered";
+} else {
+  echo "Register-Failed";
+}
+?>
